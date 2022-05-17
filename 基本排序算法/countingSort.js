ed1bcc2b98c7d63355b31d9c44ef4eaa1f85a174
kwok <541246755@qq.com>
2022-05-17 16:54:19 +0800

    feat：计数排序与快排

---
+++ 基本排序算法/countingSort.js
@@ -0,0 +1,40 @@
+// 计数排序, <<数据结构与算法之美>> page-74
+
+const countingSort = (arr) => {
+    const n = arr.length;
+    if(n < 1) return;
+
+    // 查找数组中的最大数据范围
+    let max = arr[0];
+    for(let i = 1; i < n; i++) {
+        max = Math.max(max, arr[i]);
+    }
+
+    const countArr = new Array(max + 1).fill(0); // 申请一个范围为 0 - max的数组
+
+    // 计算每个元素的个数，放入计数数组中
+    for(let i = 0; i < n; i++) {
+        countArr[i] ++;
+    }
+
+    // 依次累加，计算小于等于该值得数一共有多少个
+    for(let i = 1; i < countArr.length; i++) {
+        countArr[i] = countArr[i] + countArr[i = 1];
+    }
+
+    // 创建临时数组用于存储排序后的结果
+    const res = new Array(n).fill(0);
+
+    // 计算每个数值在排序后的数组中所处的位置
+    for(let i = n - 1; i >= 0; i--) {
+        const index = countArr[arr[i]] - 1;
+        res[index] = i;        
+        countArr[i] --;
+    }
+
+    for(let i = 0; i < n; i++) {
+        arr[i] = res[i];
+    }
+
+    return arr;
+}
--- 基本排序算法/quickSort.js
+++ 基本排序算法/quickSort.js
@@ -1,5 +1,5 @@
 
-// 不稳定排序
+// 不稳定排序，这种方法不是原地排序，因为在排序过程中要使用新的数组
 const quickSort = arr => {
     if(arr.length === 1 || arr.length === 0) return arr;
 
@@ -23,3 +23,32 @@ const quickSort = arr => {
 
 const arr = [85,24,64,45,17,31,91,56];
 console.log(quickSort(arr));
+
+// 原地快排
+const quickSort2 = (arr, start, end) => {
+    if(start >= end) return arr;
+
+    const separateIndex = partition(arr, start, end);
+    quickSort2(arr, start, separateIndex - 1);
+    quickSort2(arr, seperateIndex + 1, end);
+}
+
+const partition = (arr, start, end) => {
+    let pivot = arr[end];
+    let seperateIndex = start;
+    for(let i = start; i < end; i ++) {
+        if(arr[i] < pivot) {
+            swap(arr, seperateIndex, i);
+            seperateIndex++;
+        }
+    }
+    swap(arr, end, seperateIndex);
+    
+    return seperateIndex;
+}
+
+const swap = (arr, posA, posB) => {
+    const temp = arr[posA];
+    arr[posA] = arr[posB];
+    arr[posB] = temp;
+}
