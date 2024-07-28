be91d6c9bce6d18ec7dc3b73959e9ec4586e5c01
郭夕 <guoxi@fadada.com>
2024-07-28 09:47:09 +0800

    feat: 补充快速排序相关

--- 基本排序算法/quickSort.js
+++ 基本排序算法/quickSort.js
@@ -25,6 +25,7 @@ const arr = [85,24,64,45,17,31,91,56];
 console.log(quickSort(arr));
 
 // 原地快排
+// 快排的递推公式： quick_sort(p, r) = partion(p, r) + quick_sort(p, q - 1) + quick_sort(q, r)
 const quickSort2 = (arr, start, end) => {
     if(start >= end) return arr;
 
@@ -33,6 +34,7 @@ const quickSort2 = (arr, start, end) => {
     quickSort2(arr, seperateIndex + 1, end);
 }
 
+// 将[start....end]的数组划分为两个区间，并返回两个区间之间的边界索引
 const partition = (arr, start, end) => {
     let pivot = arr[end];
     let seperateIndex = start;
@@ -42,10 +44,15 @@ const partition = (arr, start, end) => {
             seperateIndex++;
         }
     }
+    //遍历结束之后再交换一次是因为，end出对应的数(也就是pivot)应该是大区间里面最小的那个数，也就是大区间的第一个
+    // seperateIndex对应的是为大区间的第一个数，因此需要交换两者
     swap(arr, end, seperateIndex);
     
     return seperateIndex;
 }
+// [8,7,6,5,4]
+
+// [4,5,6,7,8] -> [8,5,6,7,4]
 
 const swap = (arr, posA, posB) => {
     const temp = arr[posA];
